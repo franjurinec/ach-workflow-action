f60e95d14a0e3592b3a0c568b7df4487cb1d6c75
Fran Jurinec <franjurinec@live.com>
2023-07-04 16:04:51 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+try {
+  const INVENIO_API_URL = core.getInput('invenio-url');
+  const INVENIO_API_KEY = core.getInput('invenio-key');
+  const ROOT_RECORD = core.getInput('invenio-root-id');
+  const METADATA_FILE = core.getInput('metadata-file');
+
+  const authHeaders = {
+    Authorization: `Bearer ${INVENIO_API_KEY}`
+  }
+
+  // Get or create new draft from root record
+  let draft = fetch(new URL(`/api/records/${ROOT_RECORD}/draft`, INVENIO_API_URL), {
+    headers: authHeaders
+  }).then(res => res.json())
+  if (draft.status === 404)
+    draft = fetch(new URL(`/api/records/${ROOT_RECORD}/versions`, INVENIO_API_URL), {
+      headers: authHeaders,
+      method: 'POST'
+    }).then(res => res.json())
+
+  // Update record using metadata
+  metadataBuffer = fs.readFileSync(METADATA_FILE)
+  metadata = metadataBuffer.toString()
+  draft = fetch(new URL(`/api/records/${draft.id}/versions`, INVENIO_API_URL), {
+    headers: authHeaders,
+    method: 'PUT',
+    body: {
+      "access": {"record": "public", "files": "public"},
+      // "files": {"enabled": False},
+      "metadata": {
+          "title": metadata.title,
+          "description": metadata.description,
+          "publication_date": datetime.today().strftime('%Y-%m-%d'),
+          "creators": [
+              {
+                  "person_or_org": {
+                      "family_name": metadata.authors[0].surname,
+                      "given_name": metadata.authors[0].name,
+                      "type": "personal",
+                  },
+              },
+          ],
+          "resource_type": {"id": "dataset"},
+      },
+  }
+  }).then(res => res.json())
+
+  record = fetch(new URL(`/api/records/${draft.id}/publish`, INVENIO_API_URL), {
+    headers: authHeaders,
+    method: 'POST'
+  }).then(res => res.json())
+
+  core.setOutput('record', record.links.self)
+} catch (error) {
+  core.setFailed(error.message);
+}
